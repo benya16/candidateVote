64cd78efc187b1684196207a58fecb385f94cf70
Benjamin Carlisle <bjamin.carlisle@gmail.com>
2017-11-30 13:19:54 -0700

    Now can select from checklists

--- public/javascript/voteController.js
+++ public/javascript/voteController.js
@@ -2,4 +2,16 @@
 
     console.log("Inside vote controller");
 
+    $scope.people = [{name: 'Tyler'}, {name: 'Ben C'}, {name: 'Ben W'}];
+
+    $scope.selected = [];
+
+    $scope.submit = function () {
+        angular.forEach($scope.people, function(person) {
+            if(person.selected) {
+                console.log("Person selected: ", person);
+            }
+        });
+    }
+
 }]));
